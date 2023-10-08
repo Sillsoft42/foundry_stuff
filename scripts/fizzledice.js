d62a94f5b70107b79a48db73bd16540ca195623b
Sillsoft42 <144066598+Sillsoft42@users.noreply.github.com>
2023-10-08 20:13:43 -0300

    add fizzle dice stuff

---
+++ scripts/fizzledice.js
@@ -0,0 +1,48 @@
+Hooks.once("init", () => {
+	CONFIG.DND5E.spellProgression.myartificer = "A5E Artificer";
+	console.log("hello world");
+});
+
+Hooks.on("dnd5e.preItemUsageConsumption", async (item, consume, options, update) => {
+	if (item.type != 'spell')
+		return [item, consume, options];
+
+	if (item.system.preparation.mode === 'atwill')
+		return [item, consume, options];
+
+	const spellLvl = item.system.level;
+
+	if (spellLvl < 1) 
+		return [item, consume, options];
+
+	const actor = item.actor;
+	let adata = actor.getRollData();
+	if (Object.keys(adata.classes).length != 1)
+		return [item, consume, options];
+
+	if (adata.classes['a5e-artificer'] === undefined)
+		return [item, consume, options];
+
+	let fd = new Roll("1@scale.a5e-artificer.fizzle-dice", adata);
+
+	consume.consumeSpellLevel = false;
+	consume.consumeSpellSlot = false;
+	await fd.evaluate();
+	game.dice3d.showForRoll(fd);
+	let style = "<i>";
+	let text = "As "+actor.name+" fires off the spell, ";
+	if (fd.total <= spellLvl) {
+		style = "<i style='color:red;'>";
+		text += "the pistol jams and can no longer be used";
+	} else {
+		style = "<i style='color:green;'>";
+		text += "the pistol sparks but continues to function";
+	}
+	let chatData = {
+		speaker: ChatMessage.getSpeaker(),
+		content: style+text+"</i>"
+	};
+	ChatMessage.create(chatData);
+
+	return [item, consume, options];
+})
