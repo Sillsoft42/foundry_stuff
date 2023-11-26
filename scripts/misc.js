c89c27d4086f707e81e944420d973b8907d8f83e
Sillsoft42 <144066598+Sillsoft42@users.noreply.github.com>
2023-11-26 18:53:15 -0400

    added languages

--- scripts/misc.js
+++ scripts/misc.js
@@ -3,5 +3,6 @@ Hooks.once("init", () => {
 		label: "Intergrated Protection",
 		formula: "@attributes.ac.armor + @attributes.ac.dex + 1"
 	};
+	CONFIG.DND5E.languages.exotic.children.velf = "Valenar Elf";
+	CONFIG.DND5E.languages.exotic.children.drow = "Drow";
 });
-
