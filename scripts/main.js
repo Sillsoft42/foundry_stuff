4e547257a557a7fbee7458910869d92d83fdeb96
Sillsoft42 <144066598+Sillsoft42@users.noreply.github.com>
2025-11-08 15:09:39 -0400

    v13 update

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
-console.warn('main.js');
+console.warn('calling main.js');
 
 import {buildHooks} from "./insp.js";
+//import {activityHooks} from "./spells.js";
 //import {wardenProgression} from "./warden-progression.js";
 //import {tah_addition} from "./tah_sillsoft.js";
 
 buildHooks();
+//activityHooks();
 //tah_addition();
 //Hooks.once("init", wardenProgression);
 
+//Hooks.once("init", () => {
+//   CONFIG.Token.movement.defaultSpeed = 256; // default = 6
+//});
